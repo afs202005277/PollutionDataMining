fb72a71b9dcc10b007ffd2a01e58731c486697d0
andre <up202005277@up.pt>
2024-10-07 10:52:20 +0100

    added new dataset

--- prepare_data.py
+++ prepare_data.py
@@ -1,11 +1,34 @@
+from unittest.mock import inplace
+
 import pandas as pd
 import numpy as np
 
+abbreviation_to_state = {
+    'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California',
+    'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia',
+    'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa',
+    'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland',
+    'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri',
+    'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey',
+    'NM': 'New Mexico', 'NY': 'New York', 'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio',
+    'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina',
+    'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont',
+    'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'
+}
+
+state_to_abbreviation = {state: abbrev for abbrev, state in abbreviation_to_state.items()}
 
 def prepare_data():
     data1 = pd.read_csv('data/VAERSDATA.csv')
     data2 = pd.read_csv('data/VAERSVAX.csv')
     data3 = pd.read_csv('data/VAERSSYMPTOMS.csv')
+
+    pollution_data = pd.read_csv('data/water_air_pollution.csv')
+
+    pollution_data.columns = pollution_data.columns.str.replace(r"[\"']", "", regex=True).str.strip()
+    pollution_data = pollution_data.applymap(lambda x: x.replace('"', '').replace("'", "").strip() if isinstance(x, str) else x)
+
+    pollution_data = pollution_data[pollution_data['Country'] == 'United States of America']
 
     # merge data based on VAERS_ID
     data = pd.merge(data1, data2, on='VAERS_ID')
@@ -15,9 +38,10 @@ def prepare_data():
 
     data['hasHeadache'] = np.where(
         (data['SYMPTOM1'] == 'Headache') | (data['SYMPTOM2'] == 'Headache') | (data['SYMPTOM3'] == 'Headache') | (
-                    data['SYMPTOM4'] == 'Headache') | (data['SYMPTOM5'] == 'Headache'), 1, 0)
+                data['SYMPTOM4'] == 'Headache') | (data['SYMPTOM5'] == 'Headache'), 1, 0)
 
-    cols = ['VAERS_ID', 'CAGE_YR', 'RECVDATE', 'TODAYS_DATE', 'SYMPTOM1', 'SYMPTOM2', 'SYMPTOM3', 'SYMPTOM4', 'SYMPTOM5',
+    cols = ['VAERS_ID', 'CAGE_YR', 'RECVDATE', 'TODAYS_DATE', 'SYMPTOM1', 'SYMPTOM2', 'SYMPTOM3', 'SYMPTOM4',
+            'SYMPTOM5',
             'SYMPTOMVERSION1', 'SYMPTOMVERSION2', 'SYMPTOMVERSION3', 'SYMPTOMVERSION4', 'SYMPTOMVERSION5',
             'SYMPTOM_TEXT', 'LAB_DATA', 'CUR_ILL']
 
@@ -28,7 +52,7 @@ def prepare_data():
 
     data['VAX_DATE'] = (pd.to_datetime(data['VAX_DATE']).astype('int64') // 10 ** 9).astype('int32')
     data['ONSET_DATE'] = (pd.to_datetime(data['ONSET_DATE']).astype('int64') // 10 ** 9).astype('int32')
-    #data['TODAYS_DATE'] = (pd.to_datetime(data['TODAYS_DATE']).astype('int64') // 10 ** 9).astype('int32')
+    # data['TODAYS_DATE'] = (pd.to_datetime(data['TODAYS_DATE']).astype('int64') // 10 ** 9).astype('int32')
 
     data['RECOVD'].replace({'N': -1, 'U': 0, 'Y': 1}, inplace=True)
     data['RECOVD'].fillna(0, inplace=True)
@@ -55,10 +79,23 @@ def prepare_data():
 
     data['VAX_NAME'] = pd.factorize(data['VAX_NAME'])[0]
 
-    #data['CAGE_YR'].fillna(data['CAGE_YR'].mean(), inplace=True)
+    # data['CAGE_YR'].fillna(data['CAGE_YR'].mean(), inplace=True)
 
-    data['STATE'] = pd.factorize(data['STATE'])[0]
-    data['STATE'].fillna(-1, inplace=True)
+    data.rename(columns={'STATE': 'State'}, inplace=True)
+    pollution_data.rename(columns={'Region': 'State'}, inplace=True)
+
+    pollution_data['State'] = pollution_data['State'].replace('District of Columbia', 'Washington')
+
+    pollution_data['State'] = pollution_data['State'].map(state_to_abbreviation)
+
+    state_labels, unique_states = pd.factorize(data['State'])
+    # Step 2: Create a mapping dictionary from the unique states and their factorized labels
+    state_mapping = {state: idx for idx, state in enumerate(unique_states)}
+
+    # Step 3: Apply the same factorization to df2 using the mapping
+    pollution_data['State'] = pollution_data['State'].map(state_mapping)
+    data['State'] = data['State'].map(state_mapping)
+    pollution_data['State'].fillna(-1, inplace=True)
 
     data['NUMDAYS'].fillna(data['NUMDAYS'].mode()[0], inplace=True)
 
@@ -66,7 +103,7 @@ def prepare_data():
 
     data['ONSET_DATE'].dropna(inplace=True)
     data['VAX_DATE'].dropna(inplace=True)
-    #data['TODAYS_DATE'].dropna(inplace=True)
+    # data['TODAYS_DATE'].dropna(inplace=True)
 
     data['VAX_DOSE_SERIES'].replace('UNK', 0, inplace=True)
     data['VAX_DOSE_SERIES'].fillna(0, inplace=True)
@@ -102,10 +139,14 @@ def prepare_data():
     n = len(data[data['hasHeadache'] == 1])
 
     data = pd.concat([data[data['hasHeadache'] == 1], data[data['hasHeadache'] == 0].sample(n)])
-    
-    data = data.sample(frac = 1)
 
-    data.to_csv('data_processed.csv', index=False)
+    data = data.sample(frac=1)
+
+    pollution_data = pollution_data.groupby('State')[['AirQuality', 'WaterPollution']].mean().reset_index()
+
+    merged_df = pd.merge(data, pollution_data, on='State', suffixes=('_df1', '_df2'))
+
+    merged_df.to_csv('data_processed.csv', index=False)
 
 
 if __name__ == '__main__':
